cbb5ff73caa037cf95f4934f4c734700f2f873cc
chanwit-y <chanwit.yimneam@hotmail.com>
2022-07-29 22:13:03 +0700

    can select data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+// extern crate futures;
+// extern crate tiberius;
+// extern crate tokio_core;
+
+use std::result::{Result};
 use tiberius::AuthMethod;
-// use once_cell::sync::Lazy;
-// use std::env;
 use tiberius::{Client, Config};
 use tokio::net::TcpStream;
 use tokio_util::compat::TokioAsyncWriteCompatExt;
+// use tiberius::stmt::ResultStreamExt;
 
 // static CONN_STR: Lazy<String> = Lazy::new(|| {
 //     env::var("TIBERIUS_TEST_CONNECTION_STRING").unwrap_or_else(|_| {
@@ -16,23 +20,50 @@ use tokio_util::compat::TokioAsyncWriteCompatExt;
 async fn main() -> anyhow::Result<()> {
     // let config = Config::from_ado_string(&CONN_STR)?;
 
+    // let mut config = Config::new();
+
+    // config.host("0.0.0.0");
+    // config.port(1433);
+    // config.database("demo");
+    // config.authentication(AuthMethod::sql_server("SA", "abcABC123"));
+    // let tcp = TcpStream::connect(config.get_addr()).await?;
+    let config = get_config();
+    let tcp = get_connection(&config).await.unwrap();
+
+    tcp.set_nodelay(true)?;
+
+    let mut client = Client::connect(config, tcp.compat_write()).await?;
+    let sql = r#"
+            SELECT tbl.name AS table_name
+            FROM sys.tables tbl
+            WHERE tbl.is_ms_shipped = 0
+                AND tbl.type = 'U'
+            ORDER BY tbl.name;
+        "#;
+
+    let stream = client.simple_query(sql).await?;
+    let rows = stream.into_first_result().await?;
+
+    for row in rows {
+        let table_name: &str = row.get(0).unwrap_or_default();
+        print!("{} \n", table_name);
+    }
+
+    Ok(())
+}
+
+fn get_config() -> Config {
     let mut config = Config::new();
 
     config.host("0.0.0.0");
     config.port(1433);
     config.database("demo");
     config.authentication(AuthMethod::sql_server("SA", "abcABC123"));
-    let tcp = TcpStream::connect(config.get_addr()).await?;
 
-    tcp.set_nodelay(true)?;
+    config
+}
 
-    let mut client = Client::connect(config, tcp.compat_write()).await?;
-
-    let stream = client.query("SELECT @P1 as c1", &[&1i32]).await?;
-    let row = stream.into_row().await?.unwrap();
-
-    println!("{:?}", row);
-    // assert_eq!(Some(1), row.get(0));
-
-    Ok(())
+async fn get_connection(config: &Config) -> Result<TcpStream, std::io::Error>  {
+    let tcp = TcpStream::connect(config.get_addr()).await;
+    tcp
 }
