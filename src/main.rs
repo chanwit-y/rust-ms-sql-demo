3f4ac4946d8510d6ceffde9b5a855757da1d47bf
chanwit-y <chanwit.yimneam@hotmail.com>
2022-07-31 20:33:06 +0700

    +

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,17 @@
-// extern crate futures;
-// extern crate tiberius;
-// extern crate tokio_core;
 
-// use std::result::{Result};
-// use tiberius::AuthMethod;
-mod database;
+use rust_ms_sql_demo::utils::db::{ Database};
+// use tiberius::Client;
 
-use rust_ms_sql_demo::utils::db::Util;
-use tiberius::{Client};
+// use tokio_util::compat::TokioAsyncWriteCompatExt;
 
-// use tokio::net::TcpStream;
-use tokio_util::compat::TokioAsyncWriteCompatExt;
-// use tiberius::stmt::ResultStreamExt;
-
-
-
-// static CONN_STR: Lazy<String> = Lazy::new(|| {
-//     env::var("TIBERIUS_TEST_CONNECTION_STRING").unwrap_or_else(|_| {
-//         "server=tcp:localhost,1433;IntegratedSecurity=true;TrustServerCertificate=true".to_owned()
-//     })
-// });
-
-// #[cfg(not(all(windows, feature = "sql-browser-tokio")))]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let l = Util{name: String::from("xxxx")};
     // let config = get_config();
     // let tcp = get_connection(&config).await.unwrap();
-    // let db: Database::new();
-    let config = database::get_config();
-    let tcp = database::get_connection(&config).await.unwrap();
+    // tcp.set_nodelay(true)?;
 
-    tcp.set_nodelay(true)?;
-
-    let mut client = Client::connect(config, tcp.compat_write()).await?;
+    // let mut client = Client::connect(config, tcp.compat_write()).await?;
+    let mut db = Database::new().await;
     let sql = r#"
             SELECT tbl.name AS table_name
             FROM sys.tables tbl
@@ -41,30 +19,16 @@ async fn main() -> anyhow::Result<()> {
                 AND tbl.type = 'U'
             ORDER BY tbl.name;
         "#;
+    db.sqlect(sql).await;
+    
 
-    let stream = client.simple_query(sql).await?;
-    let rows = stream.into_first_result().await?;
+    // let stream = client.simple_query(sql).await?;
+    // let rows = stream.into_first_result().await?;
 
-    for row in rows {
-        let table_name: &str = row.get(0).unwrap_or_default();
-        print!("{} \n", table_name);
-    }
+    // for row in rows {
+    //     let table_name: &str = row.get(0).unwrap_or_default();
+    //     print!("{} \n", table_name);
+    // }
 
     Ok(())
 }
-
-// fn get_config() -> Config {
-//     let mut config = Config::new();
-
-//     config.host("0.0.0.0");
-//     config.port(1433);
-//     config.database("demo");
-//     config.authentication(AuthMethod::sql_server("SA", "abcABC123"));
-
-//     config
-// }
-
-// async fn get_connection(config: &Config) -> Result<TcpStream, std::io::Error>  {
-//     let tcp = TcpStream::connect(config.get_addr()).await;
-//     tcp
-// }
--- src/utils/db.rs
+++ src/utils/db.rs
@@ -1,13 +1,53 @@
-//  pub mod util {
+use std::result::Result;
+use tiberius::{AuthMethod, Client, Config};
+use tokio::net::TcpStream;
+use tokio_util::compat::{Compat, TokioAsyncWriteCompatExt};
 
-    pub struct Util {
-       pub name: String,
-    }
+fn get_config() -> Config {
+    let mut config = Config::new();
 
-    impl Util {
-        pub fn display_name(&self) {
-            print!("{}", self.name);
+    config.host("0.0.0.0");
+    config.port(1433);
+    config.database("demo");
+    config.authentication(AuthMethod::sql_server("SA", "abcABC123"));
+
+    config
+}
+
+async fn get_connection(config: &Config) -> Result<TcpStream, std::io::Error> {
+    let tcp = TcpStream::connect(config.get_addr()).await;
+    tcp
+}
+
+pub struct Database {
+    client: Client<Compat<TcpStream>>,
+}
+
+impl Database {
+    pub async fn new() -> Self {
+        let config = get_config();
+        let tcp = get_connection(&config).await.unwrap();
+        tcp.set_nodelay(true).unwrap();
+
+        Self {
+            client: Client::connect(config, tcp.compat_write()).await.unwrap(),
         }
     }
+
+    pub async fn sqlect(&mut self, sql: &str) {
+        let client = &mut self.client;
+        let stream = client.simple_query(sql).await.unwrap();
+        let rows = stream.into_first_result().await.unwrap();
+
+        for row in rows {
+            let table_name: &str = row.get(0).unwrap_or_default();
+            print!("{} \n", table_name);
+        }
+    }
+}
+
+// impl<'a> Default for Database<'a> {
+//     fn default() -> Self {
+//         Self::new()
+//     }
 // }
-// 
